e804ce3ea278731800ccf08c63682b3ec0ed53c0
Britany Spencergit <britany.spencer@gmail.com>
2020-11-25 14:12:19 -0300

    primera versión

--- js/app.js
+++ js/app.js
@@ -1,30 +1,94 @@
 const productos = getProductos();
 
-const carro = [];
+//////////carrito/////////
+var carro = [];
+getProductosDeLocalStorage();
+console.log(carro)
 
 const containerShop = document.getElementById('containerShop');
 containerShop.innerHTML = "";
-let contenidoContenedor = ''
+// let contenidoContenedor = ''
 
 //crea las tarjetas, las guarda en una variable y las devuelve en una función
 productos.forEach((producto) =>{
-    contenidoContenedor +=crearTarjetas(producto)
-    containerShop.innerHTML = contenidoContenedor
+    divDeProductos = crearTarjetas (producto);
+     containerShop.appendChild (divDeProductos)
 
     
 });
 
-const arrayDeBotones = document.getElementsByClassName('boton_shop');
-for(button of arrayDeBotones){
-    button.addEventListener('click', (event)=>{
-        let productoId = event.target.parentNode.id
-        let producto = productos.find (producto =>{
-            return producto.id == productoId
-            
-        })
-    console.log(producto)
-    })
+
+function crearTarjetas(producto) {
+	let divDeProductos = document.createElement('div');
+	divDeProductos.classList = 'producto col-lg-4 col-md-6 col-sm-12';
+	divDeProductos.id = producto.id;
+
+	let img = document.createElement('img');
+	img.classList = 'imagen_shop';
+	img.src = producto.img;
+	
+	let tipoDiv = document.createElement('div');
+	tipoDiv.classList = 'type';
+	tipoDiv.innerHTML = producto.type;
+
+	let precioDiv = document.createElement('div');
+	precioDiv.classList = 'price';
+	precioDiv.innerHTML = "$" + producto.price;
+
+    let stockDiv = document.createElement('div');
+    stockDiv.classList = 'stock';
+    stockDiv.innerHTML = "Pieza única"
+
+	let boton = document.createElement('button');
+	boton.classList = 'boton_shop btn btn-dark'
+	boton.innerHTML = 'Comprar'
+
+	boton.addEventListener('click', () => {
+		img.style.opacity = 0.5;
+		carro.push(producto);
+		localStorage.setItem('carro', JSON.stringify(carro))
+	});
+
+	divDeProductos.appendChild(img);
+	divDeProductos.appendChild(tipoDiv);
+	divDeProductos.appendChild(precioDiv);
+	divDeProductos.appendChild(stockDiv);
+	divDeProductos.appendChild(boton); 
+	return divDeProductos;
 }
+
+function getProductosDeLocalStorage() {
+	const carroDelLocalStorage = localStorage.getItem('carro');
+	if (carroDelLocalStorage){
+		const objetosDeLocalStorage = JSON.parse(carroDelLocalStorage);
+		objetosDeLocalStorage.forEach(objeto =>{
+			let producto = new Producto(objeto.id, objeto.type, objeto.img, objeto.price, objeto.stock)
+			carro.push(producto)
+		})
+	}else{
+		"no funciona"
+	}
+}
+
+
+
+
+
+
+
+
+
+// const arrayDeBotones = document.getElementsByClassName('boton_shop');
+// for(button of arrayDeBotones){
+//     button.addEventListener('click', (event)=>{
+//         let productoId = event.target.parentNode.id
+//         let producto = productos.find (producto =>{
+//             return producto.id == productoId
+            
+//         })
+//     console.log(producto)
+//     })
+
 
 // function crearTarjetas(producto){
 //     let codigo = `
@@ -60,52 +124,3 @@ for(button of arrayDeBotones){
 //     `
 //     return codigo;
 // }
-
-
-
-
-
-
-
-
-
-
-
-function crearTarjetas(producto) {
-	let divDeProductos = document.createElement('div');
-	divDeProductos.classList = 'producto col-lg-4 col-md-6 col-sm-12';
-	divDeProductos.id = producto.id;
-
-	let img = document.createElement('img');
-	img.classList = 'imagen_shop';
-	img.src = producto.img;
-	
-	let tipoDiv = document.createElement('div');
-	tipoDiv.classList = 'type';
-	tipoDiv.innerHTML = producto.type;
-
-	let precioDiv = document.createElement('div');
-	precioDiv.classList = 'price';
-	precioDiv.innerHTML = "$" + producto.price;
-
-    let stockDiv = document.createElement('div');
-    stockDiv.classList = 'stock';
-    stockDiv.innerHTML = "Pieza única"
-
-	let boton = document.createElement('button');
-	boton.classList = 'boton_shop btn btn-dark'
-	boton.innerHTML = 'Comprar'
-
-	boton.addEventListener('click', () => {
-		img.style.opacity = 0.5;
-		carro.push(producto);
-		console.log(carro);
-	})
-
-	divDeProductos.appendChild(img);
-	divDeProductos.appendChild(tipoDiv);
-	divDeProductos.appendChild(precioDiv);
-	divDeProductos.appendChild(stockDiv);
-	divDeProductos.appendChild(boton); 
-	return divDeProductos;
-}
